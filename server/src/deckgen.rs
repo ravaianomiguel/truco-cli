64272519bb4beb3cdfbe744ff1c597ef9f13b9a3
Miguel Ravaiano <ravaianomiguel@gmail.com>
2025-01-20 11:17:35 -0300

    deck generator

--- server/src/deckgen.rs
+++ server/src/deckgen.rs
@@ -5,33 +5,9 @@ pub fn gen_deck() -> Vec<Card> {
     let mut deck: Vec<Card> = Vec::new();
     let suits: Vec<char> = vec!['♧', '♢', '♡', '♤'];
     for n in 1..8 as u8 {
-        let num = {
-            if n <= 4 {
-                if n == 1 {
-                    'Q'
-                } else if n == 2 {
-                    'J'
-                } else if n == 3 {
-                    'K'
-                } else {
-                    'A'
-                }
-            } else if n == 7 {
-                '7'
-            } else {
-                (n + 45) as char
-            }
-        };
+        let num = find_num(n);
         for s in suits.clone() {
-            let val: u8 = {
-                if num == 'A' && s == '♤' {
-                    8
-                } else if num == '7' && s == '♡' {
-                    9
-                } else {
-                    n
-                }
-            };
+            let val: u8 = calc_val(num, s, n);
             if !(num == '7' && (s == '♧' || s == '♤')) {
                 deck.push(Card {
                     card: format!("{num}{s}"),
@@ -46,4 +22,30 @@ pub fn gen_deck() -> Vec<Card> {
     });
     deck.shuffle(&mut thread_rng());
     deck
+}
+fn find_num(n: u8) -> char {
+    if n <= 4 {
+        if n == 1 {
+            'Q'
+        } else if n == 2 {
+            'J'
+        } else if n == 3 {
+            'K'
+        } else {
+            'A'
+        }
+    } else if n == 7 {
+        '7'
+    } else {
+        (n + 45) as char
+    }
+}
+fn calc_val(num: char, s: char, n: u8) -> u8 {
+    if num == 'A' && s == '♤' {
+        8
+    } else if num == '7' && s == '♡' {
+        9
+    } else {
+        n
+    }
 }
